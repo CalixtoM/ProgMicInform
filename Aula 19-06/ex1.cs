bbde18f479bbc83711c926c370b8c1fa953387ca
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2021-06-21 17:33:57 -0300

    Exercicios 1 e 2 dia 19.

---
+++ Aula 19-06/ex1.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        double a, d, p, r, r2, p1;
+        Console.Write("Digite o valor do raio: ");
+    	r = Convert.ToDouble(Console.ReadLine());
+      	p1 = Math.PI;
+    	r2 = Math.Pow(r, 2);
+    	a = p1*r2;
+    	d = 2*r;
+    	p = 2*p1*r;
+    	Console.Write("Area: "+a+ " Diametro: "+d+ " Perimetro: " +p);
+    }
+}
---
+++ Aula 19-06/ex2.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        double a, b, b2, c, delta, delta2, x1, x2;
+        Console.Write("Digite o valor de a: ");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite o valor de b: ");
+        b = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite o valor de c: ");
+        c = Convert.ToDouble(Console.ReadLine());
+        b2 = Math.Pow(b, 2);
+        delta = b2-4*a*c;
+        delta2 = Math.Sqrt(delta);
+        x1 = (((b*(-1))+delta2)/(2*a));
+        x2 = (((b*(-1))-delta2)/(2*a));
+        Console.Write("Delta: "+delta+ ", x1: "+x1+ ", x2: " +x2);
+    }
+}
