c32b2223888aa6cd56ec1fe81417333d4742130c
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2021-06-22 11:30:35 -0300

    ex 5

---
+++ Aula 19-06/ex5.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program {
+	static void Main(string[] args) {
+		string nomecomp, ult, nome1;
+		int esp, esp1, ta;
+		Console.Write("Digite seu nome completo: ");
+		nomecomp = Console.ReadLine();
+		esp = nomecomp.IndexOf(" ");
+		esp = esp+1;
+		char[] charsToTrim = {',', '.', ' '};
+		string[] words = nomecomp.Split();
+		foreach (string word in words)
+		word.TrimEnd(charsToTrim);
+		ta = words.Length;
+		ta = ta-1;
+		esp1 = esp-1;
+		nome1 = nomecomp.Substring(0, esp1);
+		Console.Write(nome1+"."+words [ta]+"@fatec.sp.gov.br ");
+       }
+   }
