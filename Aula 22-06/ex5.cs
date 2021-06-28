4e3aab98db3b60a347387813e4993d29170ec4fb
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2021-06-28 14:55:25 -0300

    Design Exercicio 5.

--- Aula 22-06/ex5.cs
+++ Aula 22-06/ex5.cs
@@ -1,22 +1,59 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program {
-    static void Main(string[] args) {
-    	double n1, r;
-        Console.WriteLine("Digite um número: ");
-        n1 = Convert.ToDouble(Console.ReadLine());
-        if(n1 < 20){
-            Console.WriteLine("O número digitado é menor que 20");
-        	r = n1*2;
-        }
-        else if(n1 > 20){
-            Console.WriteLine("O número digitado é maior que 20");
-        	r = n1/2;
-        }
-        else{
-        	Console.WriteLine("O número digitado é igual a 20");
-            r = 20;
-        }
-        Console.Write("O resultado foi: " +r);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.SetCursorPosition(2, 2);
+            Console.WriteLine("╔══════════════════════════════════════════════╗");
+            Console.SetCursorPosition(2, 3);
+            Console.WriteLine("║                                              ║");
+            Console.SetCursorPosition(2, 4);
+            Console.WriteLine("║                                              ║");
+            Console.SetCursorPosition(2, 5);
+            Console.WriteLine("║                                              ║");
+            Console.SetCursorPosition(2, 6);
+            Console.WriteLine("║                                              ║");
+            Console.SetCursorPosition(2, 7);
+            Console.WriteLine("║                                              ║");
+            Console.SetCursorPosition(2, 8);
+            Console.WriteLine("║                                              ║");
+            Console.SetCursorPosition(2, 9);
+            Console.WriteLine("╚══════════════════════════════════════════════╝");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(20, 3);
+            Console.WriteLine("Exercicio 5!");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(4, 5);
+            double n1, r;
+        	Console.Write("Digite um número: ");
+        	n1 = Convert.ToDouble(Console.ReadLine());
+        
+        	if(n1 < 20){
+            	Console.SetCursorPosition(4, 6);
+            	Console.WriteLine("O número digitado é menor que 20");
+        		r = n1*2;
+        	}
+        	
+        	else if(n1 > 20){
+            	Console.SetCursorPosition(4, 6);
+            	Console.WriteLine("O número digitado é maior que 20");
+        		r = n1/2;
+        	}
+        	else{
+            	Console.SetCursorPosition(4, 6);
+        		Console.WriteLine("O número digitado é igual a 20");
+            	r = 20;
+        	}
+        	
+        	Console.SetCursorPosition(4, 7);
+        	Console.Write("O resultado foi: " +r);
+        }   
     }
-}
