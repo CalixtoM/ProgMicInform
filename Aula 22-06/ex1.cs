44e132cfd6d442d4ec832047dc5c4b83261b6ac8
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2021-06-28 15:43:44 -0300

    Design Exercicio 1.

--- Aula 22-06/ex1.cs
+++ Aula 22-06/ex1.cs
@@ -1,12 +1,45 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program {
-    static void Main(string[] args) {
-    	double pq, pg, r;
-        Console.WriteLine("Digite seu peso (em Kg): ");
-        pq = Convert.ToDouble(Console.ReadLine());
-		pg = (pq * 1000);
-		pg = pg+(pg*0.10);
-        Console.WriteLine("Seu peso em gramas com acréscimo de 10%: "+pg);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.SetCursorPosition(2, 2);
+            Console.WriteLine("╔═══════════════════════════════════════════════════════╗");
+            Console.SetCursorPosition(2, 3);
+            Console.WriteLine("║                                                       ║");
+            Console.SetCursorPosition(2, 4);
+            Console.WriteLine("║                                                       ║");
+            Console.SetCursorPosition(2, 5);
+            Console.WriteLine("║                                                       ║");
+            Console.SetCursorPosition(2, 6);
+            Console.WriteLine("║                                                       ║");
+            Console.SetCursorPosition(2, 7);
+            Console.WriteLine("║                                                       ║");
+            Console.SetCursorPosition(2, 8);
+            Console.WriteLine("║                                                       ║");
+            Console.SetCursorPosition(2, 9);
+            Console.WriteLine("╚═══════════════════════════════════════════════════════╝");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.SetCursorPosition(24, 3);
+            Console.WriteLine("Exercicio 1!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(4, 5);
+            double slm, sa, r;
+            Console.Write("digite o sálario minimo: ");
+            slm = Convert.ToDouble(Console.ReadLine());
+            Console.SetCursorPosition(4, 6);
+            Console.Write("digite o seu sálario: ");
+            sa = Convert.ToDouble(Console.ReadLine());        
+            r = sa/slm;
+            Console.SetCursorPosition(4, 7);
+            Console.Write("Você recebe "+Math.Round(r, 2)+" sálarios minimos");
+        }   
     }
-}
--- Aula 22-06/ex8.cs
+++ Aula 22-06/ex8.cs
@@ -1,18 +1,51 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program {
-    static void Main(string[] args) {
-    	double n1;
-        Console.WriteLine("Digite a temperatura em fahrenheit: ");
-        n1 = Convert.ToDouble(Console.ReadLine());
-        if(n1 > 0){
-            Console.WriteLine("O número digitado é positivo");
-        }
-        else if(n1<0){
-        	Console.WriteLine("O número digitado é negativo");
-        }
-        else{
-            Console.WriteLine("O número digitado é igual a zero");   
-        }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.SetCursorPosition(2, 2);
+            Console.WriteLine("╔═══════════════════════════════════════════════════════╗");
+            Console.SetCursorPosition(2, 3);
+            Console.WriteLine("║                                                       ║");
+            Console.SetCursorPosition(2, 4);
+            Console.WriteLine("║                                                       ║");
+            Console.SetCursorPosition(2, 5);
+            Console.WriteLine("║                                                       ║");
+            Console.SetCursorPosition(2, 6);
+            Console.WriteLine("║                                                       ║");
+            Console.SetCursorPosition(2, 7);
+            Console.WriteLine("║                                                       ║");
+            Console.SetCursorPosition(2, 8);
+            Console.WriteLine("║                                                       ║");
+            Console.SetCursorPosition(2, 9);
+            Console.WriteLine("╚═══════════════════════════════════════════════════════╝");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.SetCursorPosition(24, 3);
+            Console.WriteLine("Exercicio 8!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(4, 5);
+            double n1;
+            Console.Write("Digite um número: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            if(n1 > 0){
+              Console.SetCursorPosition(4, 8);
+               Console.WriteLine("O número digitado é positivo");
+            }
+            else if(n1<0){
+              Console.SetCursorPosition(4, 8);
+               Console.WriteLine("O número digitado é negativo");
+            }
+            else{
+              Console.SetCursorPosition(4, 8);
+               Console.WriteLine("O número digitado é igual a zero");   
+            }
+        }   
     }
-}
