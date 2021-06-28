a6ce529e69a88d296a6b2b9ca67c9da183b5c75d
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2021-06-28 15:12:53 -0300

    Design exercicio 3.

--- Aula 22-06/ex3.cs
+++ Aula 22-06/ex3.cs
@@ -1,11 +1,42 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program {
-    static void Main(string[] args) {
-    	double tempk, tempc;
-        Console.WriteLine("Digite a temperatura em fahrenheit: ");
-        tempk = Convert.ToDouble(Console.ReadLine());
-        tempc = (tempk - 32)*(0.555555556);
-        Console.WriteLine("A temperatura em Celsius é: "+Math.Round(tempc));       
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.SetCursorPosition(2, 2);
+            Console.WriteLine("╔═══════════════════════════════════════════════════════╗");
+            Console.SetCursorPosition(2, 3);
+            Console.WriteLine("║                                                       ║");
+            Console.SetCursorPosition(2, 4);
+            Console.WriteLine("║                                                       ║");
+            Console.SetCursorPosition(2, 5);
+            Console.WriteLine("║                                                       ║");
+            Console.SetCursorPosition(2, 6);
+            Console.WriteLine("║                                                       ║");
+            Console.SetCursorPosition(2, 7);
+            Console.WriteLine("║                                                       ║");
+            Console.SetCursorPosition(2, 8);
+            Console.WriteLine("║                                                       ║");
+            Console.SetCursorPosition(2, 9);
+            Console.WriteLine("╚═══════════════════════════════════════════════════════╝");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(24, 3);
+            Console.WriteLine("Exercicio 3!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(4, 5);
+            double tempk, tempc;
+            Console.Write("Digite a temperatura em fahrenheit: ");
+            tempk = Convert.ToDouble(Console.ReadLine());
+            tempc = (tempk - 32)*(0.555555556);
+            Console.SetCursorPosition(4, 7);
+            Console.WriteLine("A temperatura em Celsius é: "+Math.Round(tempc));
+        }   
     }
-}
