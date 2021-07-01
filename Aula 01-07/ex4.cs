789a25d64b26b8826c701edd7183998a97e48e85
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2021-07-01 17:56:31 -0300

    Exercicio 4

--- Aula 01-07/ex4.cs
+++ Aula 01-07/ex4.cs
@@ -6,8 +6,36 @@ using System;
 class Program {
     static void Main(string[] args) {
         
+        double n1, n2, n3, r;
+        Console.Write("Digite a nota AP: ");
+        n1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite a nota TP: ");
+        n2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite a nota P: ");
+        n3 = Convert.ToDouble(Console.ReadLine());
 
-        
+        n1 = (n1*0.25);
+        n2 = (n2*0.25);
+        n3 = (n3*0.50);
+
+        r = (n1+n2+n3);
+
+        if(r >= 9){
+        	Console.WriteLine(r);
+        	Console.WriteLine("Conceito A");
+        }
+        else if((r >= 7.5) && (r < 9)){
+        	Console.WriteLine(r);
+        	Console.WriteLine("Conceito B");
+        }
+        else if((r >= 6) && (r < 7.5)){
+        	Console.WriteLine(r);
+        	Console.WriteLine("Conceito C");
+        }
+        else{
+        	Console.WriteLine(r);
+        	Console.WriteLine("Conceito D");
+        }
 
     }
 }
