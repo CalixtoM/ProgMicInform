f2dc39fbf2302a70e82be28d9d8f7ec44c568420
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2021-07-01 16:28:38 -0300

    Exercicio 2 operadores lógicos.

---
+++ Aula 01-07/ex2.cs
@@ -0,0 +1,24 @@
+// 2 – Crie um programa que permite que o usuário digite o valor de um 
+// numero qualquer, verificar se o numero é múltiplo de 3 e de 7 ao 
+// mesmo tempo, caso seja imprimir na tela “Múltiplo de 3 e 7”, caso 
+// não imprimir “Não é múltiplo”.
+
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        
+
+        int n;
+        Console.Write("Digite um número: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        if((n % 3 == 0) && (n % 7 == 0)){
+        	Console.Write("Multiplo de 3 e 7");
+        }
+        else{
+        	Console.Write("Não é multiplo");
+        }
+
+    }
+}
