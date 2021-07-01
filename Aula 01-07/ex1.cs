546f598b89b30cde6ad3e5aeebb39476480ca648
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2021-07-01 17:38:22 -0300

    Exercicio 1.

---
+++ Aula 01-07/ex1.cs
@@ -0,0 +1,30 @@
+//1 – Crie um programa que permita que o usuário digite o valor das vendas que executou neste mês, 
+//imprimir na tela o premio que ganhou por estas vendas de acordo com os critérios abaixo:
+
+//•Menor que R$ 1000,00 (Sem premio).
+//•Maior igual a R$ 1000,00 e menor que R$ 3000,00 (premio de R$ 150,00).
+//•Maior igual a R$ 3000,00 (premio de R$ 300,00).
+
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        
+
+		double v;
+
+		Console.Write("Escreva o valor de vendas: ");
+		v = Convert.ToDouble(Console.ReadLine());
+
+		if(v < 1000){
+			Console.Write("Sem premios");
+		}
+		else if((v >= 1000.00) && (v < 3000.00)){
+			Console.Write("Premio de R$ 150.00");
+		}
+		else{
+			Console.Write("Premio de 300.00");
+		}
+
+    }
+}
---
+++ Aula 01-07/ex4.cs
@@ -0,0 +1,13 @@
+//4 - Faça um programa para ler as 3 notas. Calcular a média de aproveitamento, usando a fórmula abaixo e 
+// escrever o conceito do aluno de acordo com a tabela de conceitos mais abaixo:
+
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        
+
+        
+
+    }
+}
