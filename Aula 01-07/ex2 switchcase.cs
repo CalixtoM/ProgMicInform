fdc5fc551894128afb17f38ab5623c29a4432354
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2021-07-06 12:48:23 -0300

    Exercicio 2 com design.

--- Aula 01-07/ex2 switchcase.cs
+++ Aula 01-07/ex2 switchcase.cs
@@ -1,18 +1,36 @@
-//3 - Faça um programa para ler: a descrição do produto (nome), a quantidade adquirida e o preço unitário.
-//Calcular e escrever o total (total = quantidade adquirida * preço unitário), o desconto e o total a pagar
-// (total a pagar = total - desconto), sabendo-se que:
 
-// - Se quantidade <= 5 o desconto será de 2%
-
-// - Se quantidade > 5 e quantidade <=10 o desconto será de 3%
-
-// - Se quantidade > 10 o desconto será de 5%
 
 using System;
 
 class Program {
     static void Main(string[] args) {
-        
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.SetCursorPosition(2, 2);
+        Console.WriteLine("╔═══════════════════════════════════════════════════════╗");
+        Console.SetCursorPosition(2, 3);
+        Console.WriteLine("║                                                       ║");
+        Console.SetCursorPosition(2, 4);
+        Console.WriteLine("║                                                       ║");
+        Console.SetCursorPosition(2, 5);
+        Console.WriteLine("║                                                       ║");
+        Console.SetCursorPosition(2, 6);
+        Console.WriteLine("║                                                       ║");
+        Console.SetCursorPosition(2, 7);
+        Console.WriteLine("║                                                       ║");
+        Console.SetCursorPosition(2, 8);
+        Console.WriteLine("║                                                       ║");
+        Console.SetCursorPosition(2, 9);
+        Console.WriteLine("║                                                       ║");
+        Console.SetCursorPosition(2, 10);
+        Console.WriteLine("║                                                       ║");
+        Console.SetCursorPosition(2, 11);
+        Console.WriteLine("╚═══════════════════════════════════════════════════════╝");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.SetCursorPosition(24, 3);
+        Console.WriteLine("Exercicio 2!");
+        Console.ForegroundColor = ConsoleColor.White;
         int opcao;
         double n1, n2, r;
         Console.SetCursorPosition(4, 5);
@@ -23,16 +41,169 @@ class Program {
         Console.Write("3 – Multiplicação. \n");
         Console.SetCursorPosition(4, 8);
         Console.Write("4 – Divisão. \n");
-        Console.Write("Escolha uma operação: ");
+        Console.SetCursorPosition(25, 10);
+        Console.Write("( )");
+        Console.SetCursorPosition(4, 10);
+        Console.Write("Escolha uma operação:");
+        Console.SetCursorPosition(26, 10);
         opcao = Convert.ToInt32(Console.ReadLine());
         switch(opcao) {
             case 1:
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.SetCursorPosition(2, 2);
+                Console.WriteLine("╔═══════════════════════════════════════════════════════╗");
+                Console.SetCursorPosition(2, 3);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 4);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 5);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 6);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 7);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 8);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 9);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 10);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 11);
+                Console.WriteLine("╚═══════════════════════════════════════════════════════╝");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.SetCursorPosition(26, 3);
+                Console.WriteLine("Adição!");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(4, 7);
                 Console.Write("Digite o primeiro numero: ");
                 n1 = Convert.ToDouble(Console.ReadLine());
+                Console.SetCursorPosition(4, 8);
                 Console.Write("Digite o segundo numero: ");
                 n2 = Convert.ToDouble(Console.ReadLine());
                 r = n1+n2;
-                Console.Write(r);
+                Console.SetCursorPosition(12, 10);
+                Console.Write("O resultado dessa conta é: "+r);
+
+            break;
+
+            case 2:
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.SetCursorPosition(2, 2);
+                Console.WriteLine("╔═══════════════════════════════════════════════════════╗");
+                Console.SetCursorPosition(2, 3);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 4);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 5);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 6);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 7);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 8);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 9);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 10);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 11);
+                Console.WriteLine("╚═══════════════════════════════════════════════════════╝");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.SetCursorPosition(26, 3);
+                Console.WriteLine("Subtração!");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(4, 7);
+                Console.Write("Digite o primeiro numero: ");
+                n1 = Convert.ToDouble(Console.ReadLine());
+                Console.SetCursorPosition(4, 8);
+                Console.Write("Digite o segundo numero: ");
+                n2 = Convert.ToDouble(Console.ReadLine());
+                r = n1-n2;
+                Console.SetCursorPosition(12, 10);
+                Console.Write("O resultado dessa conta é: "+r);
+            break;
+
+            case 3:
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.SetCursorPosition(2, 2);
+                Console.WriteLine("╔═══════════════════════════════════════════════════════╗");
+                Console.SetCursorPosition(2, 3);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 4);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 5);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 6);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 7);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 8);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 9);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 10);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 11);
+                Console.WriteLine("╚═══════════════════════════════════════════════════════╝");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.SetCursorPosition(22, 3);
+                Console.WriteLine("Multiplicação!");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(4, 7);
+                Console.Write("Digite o primeiro numero: ");
+                n1 = Convert.ToDouble(Console.ReadLine());
+                Console.SetCursorPosition(4, 8);
+                Console.Write("Digite o segundo numero: ");
+                n2 = Convert.ToDouble(Console.ReadLine());
+                r = n1*n2;
+                Console.SetCursorPosition(12, 10);
+                Console.Write("O resultado dessa conta é: "+r);
+
+            break;
+
+            case 4:
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.SetCursorPosition(2, 2);
+                Console.WriteLine("╔═══════════════════════════════════════════════════════╗");
+                Console.SetCursorPosition(2, 3);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 4);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 5);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 6);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 7);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 8);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 9);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 10);
+                Console.WriteLine("║                                                       ║");
+                Console.SetCursorPosition(2, 11);
+                Console.WriteLine("╚═══════════════════════════════════════════════════════╝");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.SetCursorPosition(26, 3);
+                Console.WriteLine("Divisão!");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(4, 7);
+                Console.Write("Digite o primeiro numero: ");
+                n1 = Convert.ToDouble(Console.ReadLine());
+                Console.SetCursorPosition(4, 8);
+                Console.Write("Digite o segundo numero: ");
+                n2 = Convert.ToDouble(Console.ReadLine());
+                r = n1/n2;
+                Console.SetCursorPosition(12, 10);
+                Console.Write("O resultado dessa conta é: "+r);
 
             break;
         }
