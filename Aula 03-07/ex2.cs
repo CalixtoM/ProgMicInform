5a7fd8b26d53196f62f481712e7a8a2e8dbcf16c
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2021-07-07 17:35:44 -0300

    Exercicios Do/While completo

--- Aula 03-07/ex2.cs
+++ Aula 03-07/ex2.cs
@@ -5,44 +5,52 @@ class Program {
     static void Main(string[] args) {
 
         double n, s;
+        string c = "n";
         s = 0;
         do{
-            Console.Write("Escreva a 1ª nota: ");
-            n = Convert.ToDouble(Console.ReadLine());
-            if(n>= 0 && n<=10){
-                s = n;
-            }    
+            Console.Clear();
+            do{
+                Console.Write("Escreva a 1ª nota: ");
+                n = Convert.ToDouble(Console.ReadLine());
+                if(n>= 0 && n<=10){
+                    s = n;
+                }    
 
-        }while(n < 0 || n > 10);
-        do{
-            Console.Write("Escreva a 2ª nota: ");
-            n = Convert.ToDouble(Console.ReadLine());
+            }while(n < 0 || n > 10);
+            do{
+                Console.Write("Escreva a 2ª nota: ");
+                n = Convert.ToDouble(Console.ReadLine());
 
-            if(n>= 0 && n<=10){
-                s = s + n;
-            }
+                if(n>= 0 && n<=10){
+                    s = s + n;
+                }
 
-        }while(n < 0 || n > 10);
-        do{
-            Console.Write("Escreva a 3ª nota: ");
-            n = Convert.ToDouble(Console.ReadLine());
+            }while(n < 0 || n > 10);
+            do{
+                Console.Write("Escreva a 3ª nota: ");
+                n = Convert.ToDouble(Console.ReadLine());
 
-            if(n>= 0 && n<=10){
-                s = s + n;
-            }
+                if(n>= 0 && n<=10){
+                    s = s + n;
+                }
 
-        }while(n < 0 || n > 10);
-        do{
-            Console.Write("Escreva a 4ª nota: ");
-            n = Convert.ToDouble(Console.ReadLine());
+            }while(n < 0 || n > 10);
+            do{
+                Console.Write("Escreva a 4ª nota: ");
+                n = Convert.ToDouble(Console.ReadLine());
 
-            if(n>= 0 && n<=10){
-                s = s + n;
-            }
+                if(n>= 0 && n<=10){
+                    s = s + n;
+                }
 
-        }while(n < 0 || n > 10);
-        
-        Console.Write("Média: "+(s/4));
+            }while(n < 0 || n > 10);
+            
+            Console.WriteLine("Média: "+(s/4));
+
+            Console.WriteLine("Digite 's' caso deseja recomeçar: ");
+            c = Console.ReadLine();
+        }
+        while(c == "s");
 
     }
 }
